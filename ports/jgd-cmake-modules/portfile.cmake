f839df18621a48995ef8ad6e6af4f3af2d81e83d
Justen Di Ruscio <justend29@gmail.com>
2025-12-09 16:23:40 -0400

    bump jcm to 3.10.1

--- ports/jgd-cmake-modules/portfile.cmake
+++ ports/jgd-cmake-modules/portfile.cmake
@@ -1,24 +1,19 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO jgd-solutions/jgd-cmake-modules
-    REF ad6475ef30462e8cf3d6f747bad35ae97e79d407
-    SHA512 18f5539f2e4dcce932afc03cfda30d7f23e16502160f01156b440d06ad03f750b90d6e625dc53138d9b24e42b3d51fa8d15230f498573594d164f9d8797acd0a
+    REF "28161be9582d161d58836c56bcae2672a75c442e"
+    SHA512 "c28dabc96cc325ac9bb4cc9b98c4e365e1fdf721fe8050af529475c837e08c4b9da859b7fe82457ccedec34094482d62dde3d45399c6a5dd80f5bb19f389458d"
     HEAD_REF main)
-
-file(READ "${CMAKE_CURRENT_LIST_DIR}/vcpkg.json" manifest)
-string(JSON version ERROR_VARIABLE error GET "${manifest}" "version")
-if(error)
-  message(FATAL_ERROR "Failed to read version from manifest file: ${error}")
-endif()
 
 vcpkg_cmake_configure(
   SOURCE_PATH "${SOURCE_PATH}"
   OPTIONS
-    -DJCM_BUILD_TESTS=OFF
-    -DJCM_BUILD_DOCS=OFF)
+    -DJCM_ENABLE_TESTS=OFF
+    -DJCM_ENABLE_DOCS=OFF
+    -DJCM_INSTALL_VERSIONED_PATHS=OFF)
 
 vcpkg_cmake_install()
-vcpkg_cmake_config_fixup(CONFIG_PATH "share/cmake/${PORT}-${version}")
+vcpkg_cmake_config_fixup()
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE.md")
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug")
